934b6ef401ccbf7fb7f46b08bd023ba156c5e485
isomer04 <isomer008@gmail.com>
2023-03-01 10:27:09 -0500

    Worked on SamossaSelector

---
+++ SamossaSelector/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState} from 'react';
+
+const App = () => {
+  const [count , setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const updateCount = () => 
+    setCount(count + multiplier);
+  
+  return(
+    <div className='App'>
+        <div className='Header'>
+            <h1>Samosa Selector</h1>
+            <h2> Count: {count} </h2>
+            <img className='samosa' src="/samosa.png" onClick={updateCount}  />
+        </div>
+        <div className="container">
+          <div className="upgrade">
+            <h3>Double Stuffed</h3>
+            <p>2x per click</p>
+            <button>10 samosas</button>
+          </div>
+          <div className="upgrade">
+            <h3>Party Pack</h3>
+            <p>5x per click</p>
+            <button>100 samosas</button>
+          </div>
+          <div className="upgrade">
+            <h3>Full Feast</h3>
+            <p>10x per click</p>
+            <button>1000 samosas</button>
+          </div>
+          
+        </div>
+    </div>
+  )
+}
+
+export default App;
