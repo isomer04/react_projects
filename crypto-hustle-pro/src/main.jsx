f02864c92c04c504ec6d218622a9c23a738ca8ac
isomer04 <isomer008@gmail.com>
2023-05-24 22:35:19 -0400

    Added CoinDetail, DetailView, and Layout components

--- crypto-hustle-pro/src/main.jsx
+++ crypto-hustle-pro/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import Layout from "./routes/Layout.jsx";
+import DetailView from "./routes/DetailView.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} path="/" element={<App />} />
+        <Route
+          index={false}
+          path="/coinDetails/:symbol"
+          element={<DetailView />}
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
